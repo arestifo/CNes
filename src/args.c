26bec0ea130095becb9c69cda1078607530339f3
Alex Restifo <calix1999@gmail.com>
2025-02-19 17:55:16 -0500

    feat: detect default audio device sample rate

--- src/args.c
+++ src/args.c
@@ -9,9 +9,27 @@ void args_init(args_t *args) {
   else
     args->cpu_logf = NULL;
 
-  // Default APU values
-  args->apu_buf_len = 64;
-  args->sample_rate = 48000;
+  // Query default audio device sample rate. If it can't be found, use a default value
+  const u16 default_buf_len = 64;
+  const i32 default_sample_rate = 48000;
+  args->apu_buf_len = default_buf_len;
+
+  char *default_device_name;
+  SDL_AudioSpec default_spec;
+
+  if (SDL_GetDefaultAudioInfo(&default_device_name, &default_spec, 0) == 0) {
+    args->sample_rate = default_spec.freq;
+
+    printf("args_init: found audio device \"%s\" with sample_rate=%d\n", default_device_name, args->sample_rate);
+
+    SDL_free(default_device_name);
+  } else {
+    // Default APU values
+    args->sample_rate = default_sample_rate;
+
+    printf("args_init: error getting default audio device: %s\n", SDL_GetError());
+    printf("args_init: using default parameters: sample_rate=%d\n", args->sample_rate);
+  }
 }
 
 void args_destroy(args_t *args) {
